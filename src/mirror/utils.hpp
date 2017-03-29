98d62e5174b47ce765e9d187529a480ba4a6679a
Dźmitry Laŭčuk <>
2017-03-30 00:21:40 +0300

    An unused header is not included.

--- src/mirror/utils.hpp
+++ src/mirror/utils.hpp
@@ -30,7 +30,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 #include "encoding.hpp"
 #include "FileDB.hpp"
 #include <stack>
-#include <string>
 #include <utility>
 
 namespace mirror
